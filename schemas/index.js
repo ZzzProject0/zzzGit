3ea6f86fa3d0429fb3c6e60925f3dfe7bc7f6630
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-12-30 22:20:09 +1100

    test

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect = () => {
-  mongoose.connect('mongodb://test:test@13.125.42.68:27017/admin',{
+  mongoose.connect('mongodb://test:test@13.125.42.68:27017/admin', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ignoreUndefined: true,
@@ -12,4 +12,6 @@ mongoose.connection.on('error', (err) => {
   console.error('몽고디비 연결 에러', err)
 })
 
+
+// test 
 module.exports = connect
