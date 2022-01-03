8702f1769ac179337663c4c7ea5367e6273e5000
huitopia <huitopia27@gmail.com>
2022-01-03 20:09:10 +0900

    수면 기록

--- schemas/diaries.js
+++ schemas/diaries.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 const DiarySchema = new mongoose.Schema({
   diaryIdx: Number,
   yearMonth: String,
-  day: String,
+  day: Number,
   feelScore: Number,
   sleepScore: Number,
+  scoreAvg: Number,
   comment: String,
-  inputDay: Date,
+  inputDate: String,
   createdAt: String,
   updatedAt: String,
   userIdx: Number
