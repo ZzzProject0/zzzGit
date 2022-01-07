c6ca325f9a7abfa0ecf20490406d9312ade4556b
huitopia <huitopia27@gmail.com>
2022-01-07 23:56:11 +0900

    https 설정

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
+const http = require('http')
+const https = require('https')
 const port = 3000
 const path = require('path')
+const fs = require('fs')
 require("dotenv").config();
 const jwt = require('jsonwebtoken')
 const authMiddlewares = require('./middlewares/auth-middleware')
@@ -24,12 +27,22 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("hello zzz")
 })
+
+const options = { // letsencrypt로 받은 인증서 경로를 입력
+  ca: fs.readFileSync('/etc/letsencrypt/live/www.zzzback.shop/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/www.zzzback.shop/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.zzzback.shop/cert.pem')
+};
+
 app.use('/api', express.urlencoded({ extended: false }), userRouter)
 app.use('/api', express.urlencoded({ extended: false }), noticeRouter)
 app.use('/api', express.urlencoded({ extended: false }), diaryRouter)
 app.use('/api/asmr', express.urlencoded({ extended: false }), asmrRouter)
 app.use('/api', express.urlencoded({ extended: false }), scoresRouter)
 
-app.listen(port, () => {
-  console.log(`listening at http://18.117.86.112:${port}`)
-})
+http.createServer(app).listen(port);
+https.createServer(options, app).listen(443);
+
+// app.listen(port, () => {
+//   console.log(`listening at http://18.117.86.112:${port}`)
+// })
