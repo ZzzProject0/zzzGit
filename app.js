aa46ad18931625571cb6f25841fd9e468aa7929f
huitopia <huitopia27@gmail.com>
2022-01-12 20:15:11 +0900

    kakao login 추가

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const passportConfig = require("./passport");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
-const http = require('http');
-const https = require('https');
+passportConfig();
 
+const http = require("http");
+const https = require("https");
+const passport = require("passport");
+const session = require("express-session");
 const port = 3000;
-const path = require('path');
-const fs = require('fs');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const authMiddlewares = require('./middlewares/auth-middleware');
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config();
 
-const userRouter = require('./routes/user');
-const noticeRouter = require('./routes/notice');
-const diaryRouter = require('./routes/diary');
-const asmrRouter = require('./routes/asmr');
-const playlistRouter = require('./routes/playlist');
+const userRouter = require("./routes/user");
+const noticeRouter = require("./routes/notice");
+const diaryRouter = require("./routes/diary");
+const asmrRouter = require("./routes/asmr");
+const playlistRouter = require("./routes/playlist");
+const authRouter = require("./routes/auth");
+const scoresRouter = require("./routes/score");
 
-const scoresRouter = require('./routes/score');
-
-const connect = require('./schemas');
+const connect = require("./schemas");
 
 connect();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('hello zzz');
+app.get("/", (req, res) => {
+  res.send("Hello Zzz");
 });
 
 // const options = { // letsencrypt로 받은 인증서 경로를 입력
@@ -39,12 +50,17 @@ app.get('/', (req, res) => {
 //   cert: fs.readFileSync('/etc/letsencrypt/live/www.zzzback.shop/cert.pem'),
 // };
 
-app.use('/api', express.urlencoded({ extended: false }), userRouter);
-app.use('/api', express.urlencoded({ extended: false }), noticeRouter);
-app.use('/api', express.urlencoded({ extended: false }), diaryRouter);
-app.use('/api/asmr', express.urlencoded({ extended: false }), asmrRouter);
-app.use('/api', express.urlencoded({ extended: false }), scoresRouter);
-app.use('/api/playlists', express.urlencoded({ extended: false }), playlistRouter);
+app.use("/api", express.urlencoded({ extended: false }), userRouter);
+app.use("/api", express.urlencoded({ extended: false }), noticeRouter);
+app.use("/api", express.urlencoded({ extended: false }), diaryRouter);
+app.use("/api/asmr", express.urlencoded({ extended: false }), asmrRouter);
+app.use("/api", express.urlencoded({ extended: false }), scoresRouter);
+app.use(
+  "/api/playlists",
+  express.urlencoded({ extended: false }),
+  playlistRouter
+);
+app.use("/auth", express.urlencoded({ extended: false }), authRouter);
 
 // http.createServer(app).listen(port);
 // https.createServer(options, app).listen(443);
