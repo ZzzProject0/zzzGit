78109f22dba78c07d7c2a3ee3502935f785c2862
huitopia <huitopia27@gmail.com>
2022-01-13 23:27:59 +0900

    test 배포

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 // require("dotenv").config();
@@ -8,14 +8,16 @@ const dev1 = process.env.DEV;
 const connect = () => {
   // PRODUCTION = "mongodb://test:test@54.180.109.58:27017"
   // DEV = "mongodb://localhost/hanghae4"
-  mongoose.connect(dev1, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).catch((err) => console.log((err)));
+  mongoose
+    .connect(production, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => console.log(err));
 };
 
-mongoose.connection.on('error', (err) => {
-  console.error('몽고디비 연결 에러', err);
+mongoose.connection.on("error", (err) => {
+  console.error("몽고디비 연결 에러", err);
 });
 
 // test
