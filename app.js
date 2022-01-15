161333094324091e13f81cc48b1819fd56e14356
huitopia <huitopia27@gmail.com>
2022-01-15 12:28:55 +0900

    .

--- app.js
+++ app.js
@@ -54,12 +54,12 @@ app.get("/", (req, res) => {
   res.send("Hello ZZZ");
 });
 
-// const options = {
-//   // letsencrypt로 받은 인증서 경로를 입력
-//   ca: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/fullchain.pem"),
-//   key: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/cert.pem"),
-// };
+const options = {
+  // letsencrypt로 받은 인증서 경로를 입력
+  ca: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/www.zzzback.shop/cert.pem"),
+};
 
 app.use("/api", express.urlencoded({ extended: false }), userRouter);
 app.use("/api", express.urlencoded({ extended: false }), noticeRouter);
@@ -74,10 +74,10 @@ app.use(
 app.use("/auth", express.urlencoded({ extended: false }), authRouter);
 app.use("/api", express.urlencoded({ extended: false }), pushRouter);
 
-// http.createServer(app).listen(port);
-// https.createServer(options, app).listen(443);
+http.createServer(app).listen(port);
+https.createServer(options, app).listen(443);
 
-app.listen(port, () => {
-  console.log(`listening at http://18.117.86.112:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`listening at http://18.117.86.112:${port}`);
+// });
 module.exports = app;
