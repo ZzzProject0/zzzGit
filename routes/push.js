81082d8c80752107b86d875ddd7bc9c7c269488e
huitopia <huitopia27@gmail.com>
2022-01-15 17:27:52 +0900

    push update

--- routes/push.js
+++ routes/push.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const webpush = require("web-push");
+const admin = require("../admin");
 
 router.post("/notifications/subscribe", async (req, res) => {
   console.log(req.body);
@@ -16,4 +17,29 @@ router.post("/notifications/subscribe", async (req, res) => {
     .catch((e) => console.log(e.stack));
   res.status(200).json({ success: true });
 });
+
+router.get("/location", (req, res) => {
+  const { registrationToken } = req.body;
+  const message = {
+    data: {
+      score: "850",
+      time: "2:45",
+    },
+    token: registrationToken,
+  };
+
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log("Successfully sent message:", response);
+      res.send(response);
+    })
+    .catch((error) => {
+      console.log("Error sending message:", error);
+      res.send(error);
+    });
+});
+
 module.exports = router;
