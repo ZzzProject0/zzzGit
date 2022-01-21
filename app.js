bbd6affa9629fbff8fa7ea995a601d491e8f2039
huitopia <huitopia27@gmail.com>
2022-01-22 08:58:04 +0900

    jenkins 17

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(
 // );
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Zzz API server test 16");
+  res.send("Welcome to Zzz API server test 17");
 });
 
 // const options = {
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "zzzGit", //자신이 만든 프로젝트 이름
-      script: "./app.js",
+      script: "npm",
       args: "start",
     },
   ],
