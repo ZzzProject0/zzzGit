155b98de0b03634d9633c785b5e569ad9d7f8e60
huitopia <huitopia27@gmail.com>
2022-01-22 04:43:35 +0900

    .env seoul 추가

--- schemas/index.js
+++ schemas/index.js
@@ -4,12 +4,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 // require("dotenv").config();
 const production = process.env.PRODUCTION;
+const production1 = process.env.PRODUCTION;
 const dev1 = process.env.DEV;
 const connect = () => {
   // PRODUCTION = "mongodb://test:test@54.180.109.58:27017"
   // DEV = "mongodb://localhost/hanghae4"
   mongoose
-    .connect(production, {
+    .connect(production1, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
